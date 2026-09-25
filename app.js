137f60bdd26a0a5e726d98c8cc52760eb1a7d5a4
iphone-pro-brasil <thecracker0day@gmail.com>
2026-09-25 16:10:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 const CONFIG = {
   API_URL: "https://api-pluspix.squareweb.app",
-  CLIENT_ID: "COLOQUE_SEU_CLIENT_ID_AQUI",
-  CLIENT_SECRET: "COLOQUE_SEU_CLIENT_SECRET_AQUI"
+  CLIENT_ID: "live_eec90e7e0609f9de846014d41961db8a",
+  CLIENT_SECRET: sk_af8ad02acee2cfea842fc4525b6ea9edb8060476c7d650a0f26827567c664ace"
 };
 
 const $ = (id) => document.getElementById(id);
